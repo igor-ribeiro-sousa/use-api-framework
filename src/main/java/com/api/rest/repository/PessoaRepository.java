f45fbe767565a96b1e98a7f4a2a5d340ff25d3ad
Igor Ribeiro <igorribeirocons@gmail.com>
2026-02-19 23:59:41 -0300

    Correção repositories

--- src/main/java/com/api/rest/repository/PessoaRepository.java
+++ src/main/java/com/api/rest/repository/PessoaRepository.java
@@ -1,6 +1,9 @@
 package com.api.rest.repository;
 
+import org.springframework.stereotype.Repository;
+
 import com.api.rest.entidade.Pessoa;
 import com.crud.framework.repository.GenericRepository;
 
+@Repository
 public interface PessoaRepository extends GenericRepository<Pessoa, Long> {}
