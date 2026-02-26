0ff205bab81e92f3b4d2880eb9140d370f916558
Igor Ribeiro <igorribeirocons@gmail.com>
2026-02-26 10:51:17 -0300

    Correção applicationTests

--- src/test/java/com/api/rest/ApiRestApplicationTests.java
+++ src/test/java/com/api/rest/ApiRestApplicationTests.java
@@ -1,10 +1,10 @@
-package com.api.produtoRest;
+package com.api.rest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProdutoRestApplicationTests {
+class ApiRestApplicationTests {
 
 	@Test
 	void contextLoads() {
